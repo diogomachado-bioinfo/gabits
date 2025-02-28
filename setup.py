f31df5f3fe81002317807ce192c6bca923fe8e2b
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2025-02-27 22:31:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gabits',
-      version='1.0.0.0',
+      version='1.0.0.1',
       author='Diogo de J. S. Machado',
       author_email='diogomachado.bioinfo@gmail.com',
       description=('Genetic algorithm implementation based on binary '
